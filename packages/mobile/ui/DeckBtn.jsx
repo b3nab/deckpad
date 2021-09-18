a3783d7537df22899b147a40893159f8e0743fd1
b3nab <3042431+b3nab@users.noreply.github.com>
2021-09-18 18:03:25 +0200

    mobile - fix react-native errors on :hover, remove vibration and use expo-haptics to give a smoother feedback on btn tap

--- packages/mobile/ui/DeckBtn.jsx
+++ packages/mobile/ui/DeckBtn.jsx
@@ -1,7 +1,18 @@
 import React, { Fragment } from 'react'
-import { Vibration, Image } from 'react-native'
-import { Caption } from 'react-native-paper'
+// import { Image } from 'react-native'
+import { impactAsync } from 'expo-haptics'
+import { Caption as RNCaption } from 'react-native-paper'
 import styled from 'styled-components/native'
+
+const Image = styled.Image`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+`
+const Caption = styled(RNCaption)`
+  color: ${props => props.color || '#FFFFFF'};
+  z-index: 1;
+`
 
 const DeckBtnWrapper = styled.TouchableHighlight`
   width: 70px;
@@ -13,41 +24,50 @@ const DeckBtnWrapper = styled.TouchableHighlight`
   align-items: center;
   overflow: hidden;
   margin: 10px;
+  border: 1px solid #ffffff60;
+  /* 
+    cannot use box-shadow property on react-native
+    need to find a solution for the 3d button effect
+   */
+  /* box-shadow: #000000 -3px 4px 0px 1px, #5c5c5c -4px 5px 0px 1px; */
   
-  ${props => props.background ? `
-    background: ${props.background};
-  ` : ''}
+  background: ${props => props.background || '#FFFFFF'};
 
-  ${props => props.shape === 'circle' && `
-    border-radius: 100px;
-  `}
-  ${props => props.shape === 'square' && `
-    border-radius: 10px;
-  `}
-
-  &:hover {
-    cursor: grab;
-  }
+  border-radius: ${props =>
+    props.shape == 'circle' ? `100px` :
+    props.shape == 'square' ? '10px' :
+    '0'
+  };  
 `
 
 export const DeckBtn = ({api, position, changeDeck, ...props}) => {
-  const { label, labelColor, shape, bgColor, image, action } = props
+  let { label, labelColor, shape, bgColor, image, action } = props
+  // console.log(JSON.stringify(props, null, 2))
+
+  const parseColor = (color) => {
+    if(typeof color == 'object') {
+      return `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`
+    }
+    return color
+  }
+
+  labelColor = parseColor(labelColor)
+  bgColor = parseColor(bgColor)
+  
 
   const fireAction = () => {
-      Vibration.vibrate([0, 50])
-      console.log('fire btn @position ', position)
-      console.log(`action => ${JSON.stringify(action,null,2)}`)
-      if(action.plugin === 'companion') {
-        if(action.type === 'change-deck') {
-            changeDeck(action.options)
-        }
+    impactAsync()
+    api.emit('action', {
+      position,
+      action,
+    })
+    // console.log('fire btn @position ', position)
+    // console.log(`action => ${JSON.stringify(action,null,2)}`)
+    if(action.plugin === 'companion') {
+      if(action.type === 'change-deck') {
+        changeDeck(action.options)
       }
-      api.post('/action', {
-          body: {
-              position,
-              action,
-          }
-      })
+    }
   }
 
   return (
@@ -58,10 +78,10 @@ export const DeckBtn = ({api, position, changeDeck, ...props}) => {
     >
         <Fragment>
             {!!image && 
-                <Image source={{uri: image}} alt="btnImage" style={{width: 70, height: 70, position: 'absolute'}}/>
+                <Image source={{uri: image}}></Image>
             }
             {!!label &&
-                <Caption style={{color: labelColor, zIndex: 1}}>{label}</Caption>
+                <Caption color={labelColor}>{label}</Caption>
             }
         </Fragment>
     </DeckBtnWrapper>
